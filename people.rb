4e481a019837e933c7a645b98c108cd988ac4c96
MitsuyoshiToguri <seiun340066@gmail.com>
2014-10-23 16:26:02 -0400

    This is my third commit , watch @ mark.

---
+++ people.rb
@@ -0,0 +1,36 @@
+class Person
+
+	def initialize(name)
+		@name = name 
+	end
+
+	def greeting
+		puts "Hi, my name is #{@name}" #instance 
+	end
+
+end
+
+class Student < Person
+
+	def learn
+	 puts ("I get it!")
+	end
+end
+
+
+class Instructor < Person
+
+	def teach
+	  puts ("Everything in Ruby is an Object")	
+	end
+end
+
+
+student = Student.new "Cristina"
+student.greeting
+student.learn
+student.inspect
+
+instructor = Instructor.new "Chris"
+instructor.greeting 
+instructor.teach
